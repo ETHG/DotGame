0524dd8dbaa3f7040d0624f0091454a7d742087f
ETHG <skielias@aim.com>
2018-05-22 14:07:11 -0700

    Removed temp testing code

--- src/Board.java
+++ src/Board.java
@@ -1,19 +1,4 @@
-//import java.awt.Color;
-//import java.awt.Dimension;
-//import java.awt.EventQueue;
-//import java.awt.Graphics;
-//import java.awt.GridLayout;
-//import java.awt.event.MouseAdapter;
-//import java.awt.event.MouseEvent;
-//
-//import javax.swing.BorderFactory;
-//import javax.swing.JFrame;
-//import javax.swing.JPanel;
-//
-//import javax.swing.*;
-//import java.util.ArrayList;
 import javax.swing.*;
-import java.awt.*;
 import java.util.Scanner;
 
 public class Board {
@@ -48,62 +33,12 @@ public class Board {
         }
     }
 
-    //Not my code:
-
     public void init() {
         JFrame frame = new JFrame("FrameDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        //frame.add(something);
         frame.setLocationRelativeTo(null);
-        frame.add(createPixels());
         frame.setVisible(true);
-    }
-
-    private JPanel createPixels() {
-        int width = 30;
-        int height = 20;
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(height, width, 0, 0));
-
-        for (int row = 0; row < height; row++) {
-            for (int column = 0; column < width; column++) {
-                MoreTesting.PixelPanel pixelPanel = new MoreTesting.PixelPanel();
-                panel.add(pixelPanel);
-            }
-        }
-
-        return panel;
-    }
-
-    public class PixelPanel extends JPanel {
-
-        private static final long serialVersionUID = 8465814529701152253L;
-
-        private static final int PIXEL_SIZE = 20;
-
-        private Color backgroundColor;
-
-        public PixelPanel() {
-            this.backgroundColor = Color.WHITE;
-            this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-            this.setPreferredSize(new Dimension(PIXEL_SIZE, PIXEL_SIZE));
-        }
-
-        public Color getBackgroundColor() {
-            return backgroundColor;
-        }
-
-        public void setBackgroundColor(Color backgroundColor) {
-            this.backgroundColor = backgroundColor;
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-
-            g.setColor(getBackgroundColor());
-            g.fillRect(0, 0, getWidth(), getHeight());
-        }
     }
 }
