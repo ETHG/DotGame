2f1441b41af12859d144e348036fc699964404f9
bcearly <ben.early44@gmail.com>
2018-06-05 11:53:02 -0700

    I changed the Look and Feel to make it adaptable across operating systems.

--- src/DotGame.java
+++ src/DotGame.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class DotGame {
     public static void main(String[] args) {
+        setLookAndFeel();
         int players = players();
         Board board = new Board(boardSize(), players);
         board.initPlayers();
@@ -26,5 +27,20 @@ public class DotGame {
         int parsedSizeInt = Integer.parseInt(size) *2;
 
         return parsedSizeInt;
+    }
+
+    public static void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getCrossPlatformLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException e) {
+        }
+        catch (ClassNotFoundException e) {
+        }
+        catch (InstantiationException e) {
+        }
+        catch (IllegalAccessException e) {
+        }
     }
 }
