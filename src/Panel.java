ce2d68cf49c7a8250e9d8078bb51e5c009dd83f3
ETHG <skielias@aim.com>
2018-06-08 15:33:28 -0700

    re added custom pannel class

---
+++ src/Panel.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Panel extends JPanel{
+    private int r;
+    private int c;
+
+
+    public Panel(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public int getC() {
+        return c;
+    }
+}
