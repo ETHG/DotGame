97fa291b95d7d4196d6a332d67b998adf105d193
ETHG <skielias@aim.com>
2018-06-14 11:39:07 -0700

    Revert "I finally figured out how to center the frame when it is created."
    This reverts commit ae7a991
    
    Test

--- src/Board.java
+++ src/Board.java
@@ -49,8 +49,6 @@ public class Board {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setSize(700, 700);
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
     }
 
     //Populates the frame w/ panels/etc
