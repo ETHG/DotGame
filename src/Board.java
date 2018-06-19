e0f1ecf853d61d3e7c04c23e09840f8b48702de3
ETHG <skielias@aim.com>
2018-06-19 10:51:06 -0700

    Did lots of stuff... whoops. Got points to work more or less correctly, double square finding, who won, etc.

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -40,8 +41,9 @@ public class Board {
     }
 
     //Creates the frame;
+    JFrame frame;
     public void init() {
-        JFrame frame = new JFrame("Dot Game");
+        frame  = new JFrame("Dot Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         JLabel turnLabel = addLabelToPane(frame.getContentPane());
@@ -49,16 +51,20 @@ public class Board {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setSize(700, 700);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
     }
 
     //Populates the frame w/ panels/etc
-    private JPanel createGrid(JLabel turnLabel) {
+    private JPanel createGrid(JLabel turnLabel)  {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(size, size));
 
         for (int r = 1; r < size; r++) {
             for (int c = 1; c < size; c++) {
                 if (r%2 == 1) {
+//                    if ((r == 1 && (c == 1 || c == 3 || c ==5)) || (c == 1 && (r == 1 || r == 3 || r == 5)))
+//                        grid[r-1][c-1] = 3;
                     if (c%2 == 0) {
                         makeButton(r, c, panel, turnLabel);
                     } else {
@@ -93,91 +99,147 @@ public class Board {
         temp[0] = r-1;
         temp[1] = c-1;
         button.addActionListener(e -> {
-            button.setBackground(Color.GRAY);
-            grid[temp[0]][temp[1]] = 1;
-            int output = checkForSquares(temp[0], temp[1]);
-            if (output == 0) {
-                logic.advanceTurn(turnLabel);
+            if (grid[temp[0]][temp[1]] != 0) {
+                //Does nothing bc if its presed when its already been pressed ignore
             } else {
-                for (int i = 0; i <= panelList.size()-1; i++) {
-                    if (output == 1 || output == 5) {
-                        if (panelList.get(i).getR() == r + 1 && panelList.get(i).getC() == c) {
-                            panelList.get(i).setBackground(logic.determineColor());
+                button.setBackground(Color.GRAY);
+                grid[temp[0]][temp[1]] = 1;
+                ArrayList<Integer> output = checkForSquares(temp[0], temp[1]);
+                if (output.size() == 0) {
+                    logic.advanceTurn(turnLabel);
+                } else {
+                    for (int i = 0; i <= panelList.size()-1; i++) {
+                        if (output.contains(1) || output.contains(5)) {
+                            if (panelList.get(i).getR() == r + 1 && panelList.get(i).getC() == c) {
+                                panelList.get(i).setBackground(logic.determineColor());
+                            }
                         }
-                    } else if (output == 2 || output == 8) {
-                        if (panelList.get(i).getR() == r - 1 && panelList.get(i).getC() == c) {
-                            panelList.get(i).setBackground(logic.determineColor());
+                        if (output.contains(2) || output.contains(8)) {
+                            if (panelList.get(i).getR() == r - 1 && panelList.get(i).getC() == c) {
+                                panelList.get(i).setBackground(logic.determineColor());
+                            }
                         }
-                    } else if (output == 3 || output == 6) {
-                        if (panelList.get(i).getR() == r && panelList.get(i).getC() == c-1) {
-                            panelList.get(i).setBackground(logic.determineColor());
+                        if (output.contains(3) || output.contains(6)) {
+                            if (panelList.get(i).getR() == r && panelList.get(i).getC() == c-1) {
+                                panelList.get(i).setBackground(logic.determineColor());
+                            }
                         }
-                    } else if (output == 4 || output == 7) {
-                        if (panelList.get(i).getR() == r && panelList.get(i).getC() == c+1) {
-                            panelList.get(i).setBackground(logic.determineColor());
+                        if (output.contains(4) || output.contains(7)) {
+                            if (panelList.get(i).getR() == r && panelList.get(i).getC() == c+1) {
+                                panelList.get(i).setBackground(logic.determineColor());
+                            }
                         }
                     }
+                    players[logic.getTurn()%playerCount].addPoint();
                 }
+                System.out.println("checking");
+                if (isEndGame()) {
+                    System.out.println("check true");
+                    endGame();
+                }
+//            for (int a = 0; a < grid.length; a++)
+//                System.out.println( Arrays.toString(grid[a]));
+//            System.out.println("");
             }
         });
         panel.add(button);
     }
 
-    public int checkForSquares(int r, int c) {
+    public ArrayList<Integer> checkForSquares(int r, int c) {
+        ArrayList<Integer> squaresFound = new ArrayList<>();
         if (r <= 0) {
             //top case
             if (grid[r+1][c-1] == 1 && grid[r+1][c+1] == 1 && grid[r+2][c] == 1) {
                 System.out.println("found square! v1");
-                return 1;
+                squaresFound.add(1);
+                //return 1;
             }
         } else if (r == grid.length-1) {
             //bottom case
             if (grid[r-1][c-1] == 1 && grid[r-1][c+1] == 1 && grid[r-2][c] == 1) {
                 System.out.println("found square v2");
-                return 2;
+                squaresFound.add(2);
+                //return 2;
             }
         } else if (c == grid.length-1) {
             //right case
             if (grid[r-1][c-1] == 1 && grid[r+1][c-1] == 1 && grid[r][c-2] == 1) {
                 System.out.println("found square v3");
-                return 3;
+                squaresFound.add(3);
+                //return 3;
             }
         } else if (c == 0) {
             //left case
             if (grid[r + 1][c + 1] == 1 && grid[r - 1][c + 1] == 1 && grid[r][c + 2] == 1) {
                 System.out.println("found square v4");
-                return 4;
+                squaresFound.add(4);
+                //return 4;
             }
         } else {
             if (r % 2 == 0) {
                 if (grid[r + 1][c - 1] == 1 && grid[r + 1][c + 1] == 1 && grid[r + 2][c] == 1 && grid[r + 1][c] != 3) {
                     System.out.println("found square! v5");
-                    return 5;
+                    squaresFound.add(5);
+                    //return 5;
                 }
-                if (grid[r-1][c-1] == 1 && grid[r-1][c+1] == 1 && grid[r-2][c] == 1 && grid[r-1][c] != 3) {
+                if (grid[r - 1][c - 1] == 1 && grid[r - 1][c + 1] == 1 && grid[r - 2][c] == 1 && grid[r - 1][c] != 3) {
                     System.out.println("fs v8");
-                    return 8;
+                    squaresFound.add(8);
+                    //return 8;
                 }
             } else {
                 if (grid[r - 1][c - 1] == 1 && grid[r + 1][c - 1] == 1 && grid[r][c - 2] == 1 && grid[r][c - 1] != 3) {
                     System.out.println("fs v6");
-                    return 6;
+                    squaresFound.add(6);
+                    //return 6;
                 }
 
                 if (grid[r + 1][c + 1] == 1 && grid[r - 1][c + 1] == 1 && grid[r][c + 2] == 1 && grid[r][c + 1] != 3) {
                     System.out.println("fs v7");
-                    return 7;
+                    squaresFound.add(7);
+                    //return 7;
                 }
             }
         }
-        return 0;
+        return squaresFound;
     }
-
 
     public JLabel addLabelToPane(Container pane) {
         JLabel turnLabel = new JLabel("It is " + players[0].getName() + "'s turn.");
         turnLabel.setFont(new Font("Helvetica", Font.BOLD, 30));
         pane.add(turnLabel, BorderLayout.PAGE_END);
         return  turnLabel;
+    }
+
+    public boolean isEndGame() {
+        for (int r = 0; r < grid.length; r++)
+            for (int c = 0; c < grid.length; c++) {
+                if (grid[r][c] == 0)
+                    if (!((r%2==0 && c==0) || (c%2==0 && r==0)))
+                        return false;
+            }
+        return true;
+    }
+
+    public void endGame() {
+        Player max = new Player("random", 100);
+        for (int i = 0; i < playerCount; i++) {
+            if (max.getPoints() < players[i].getPoints()) {
+                max = players[i];
+                //System.out.println("" + players[i].getName());
+            } else if (max.getPoints() == players[i].getPoints()) //Todo tie dosent work quite right bc its not evaluated on completeion
+                max = new Player("tie", 1000);
+        }
+        if (max.getId() == 1000) {
+            JOptionPane.showMessageDialog(null, "It's a tie!","Message", JOptionPane.INFORMATION_MESSAGE);
+        } else
+            JOptionPane.showMessageDialog(null, "Player " + max.getName() + " wins with " + max.getPoints() + " points!","Message", JOptionPane.INFORMATION_MESSAGE);
+        int reply = JOptionPane.showConfirmDialog(null, "Want to play a gain? ", "Play again?", JOptionPane.YES_NO_OPTION);
+        if (reply == JOptionPane.YES_OPTION) {
+//            String[] arguments = new String[] {"123"};
+//            DotGame.main(arguments);  todo handel exceptions thrown by this
+        } else {
+            frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+        }
     }
 }
